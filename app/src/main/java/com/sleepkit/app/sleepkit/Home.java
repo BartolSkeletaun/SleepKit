6a194f67fd855a1fbdc93d3ef075bc168a292336
BartolSkeletaun <barry.stapleton@gmail.com>
2019-01-13 21:06:48 +0000

    Have added enter the number of seconds to play - this accepts a user input string parsed to an interger - this begins a countdown timer upon reaching the end the autopause method is called stopping the audio
    Also have added comments to the home.activity

--- app/src/main/java/com/sleepkit/app/sleepkit/Home.java
+++ app/src/main/java/com/sleepkit/app/sleepkit/Home.java
@@ -4,21 +4,26 @@ import android.media.AudioAttributes;
 import android.media.AudioManager;
 import android.media.SoundPool;
 import android.os.Build;
-import android.support.annotation.RequiresApi;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.media.MediaPlayer;
 import android.view.View;
+import android.widget.TextView;
 
-import java.lang.annotation.Target;
 
 import static android.media.AudioAttributes.CONTENT_TYPE_MUSIC;
 import static android.media.AudioAttributes.USAGE_MEDIA;
 
 public class Home extends AppCompatActivity {
 
+    // We will use a soundPool to handle Audio
+
     SoundPool soundPool;
+
+    //Declare an integer variable for the SoundPool to play representing the Audio in raw folder
+
     private int sound1;
+
 
 
     @Override
@@ -26,7 +31,11 @@ public class Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        //Some version control
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+
+            // new Audio Attributes builder to assign audio type
 
             AudioAttributes audioAttributes = new AudioAttributes.Builder()
                     .setUsage(USAGE_MEDIA)
@@ -38,25 +47,66 @@ public class Home extends AppCompatActivity {
                     .build();
 
         } else {
+
+            //Some more version control
+
             soundPool = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
         }
+
+        //Load our audio from raw folder to sound1 integer
+
         sound1 = soundPool.load(this, R.raw.bs1, 1);
 
 
     }
 
+    //On click method to start Audio - loop set to -1 for infinite looping
 
     public void playAudio(View view) {
+
         switch (view.getId()) {
             case R.id.button_play:
                 soundPool.play(1, 1, 1, 0, -
                         1, 1);
                 break;
-
         }
+
+        //Some code to handle user input to edit text view count dowm timer
+
+        TextView countdownText1 = findViewById(R.id.text_countdown);
+
+        // declare string to accept user time input
+
+        String myString = ((TextView)countdownText1).getText().toString();
+
+        //create new variable countdownText1 to parse string
+
+        int countdownMillis = Integer.parseInt(myString) * 1000;
+
+        //parse input string to integer and multiply * 1000 for conversion to miliseconds
+
+        //declare new countdown timer - pass the number of millis to the count down timer
+
+            new CountDownTimer(countdownMillis, 1000) {
+              TextView countdownText = findViewById(R.id.text_countdown);
+                public void onTick(long millisUntilFinished) {
+                    countdownText.setText("seconds remaining: " + millisUntilFinished / 1000);
+                }
+
+         // calling the onFinish method = on calling this method pause(stop) the audio stream
+
+               public void onFinish() {
+                    countdownText.setText("done!");
+                    soundPool.autoPause();
+                }
+            }.start();
+
+
 
 
     }
+
+    //code to handle pause button press
 
     public void pauseAudio(View view1) {
         switch (view1.getId()) {
